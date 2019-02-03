d40fd389ed8aca70f4db16963b3fef76944ac4c0
paulinare <43135750+paulinare@users.noreply.github.com>
2019-02-03 19:31:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,28 +18,28 @@ var product_details = {
     },
     
     display_one_product: function() {
-        var parent = document.getElementById('order-cart');
-        var line = document.createElement('div');
-        line.classList.add('line');
-        var container = document.createElement('ul');
+        var parent = document.getElementById("order-cart");
+        var line = document.createElement("div");
+        line.classList.add("line");
+        var container = document.createElement("ul");
         parent.appendChild(container); 
-        var product_div = document.createElement('div');                //product name div
+        var product_div = document.createElement("div");                //product name div
         product_div.classList.add("product_name_box");
         container.appendChild(product_div);
-        var product_label = document.createElement('li');
+        var product_label = document.createElement("li");
         product_label.innerHTML = "Product";
         product_div.appendChild(product_label);
         product_label.classList.add("small-screen-label");
         product_label.classList.add("product");
-        var product_name = document.createElement('p');
+        var product_name = document.createElement("p");
         product_name.classList.add("product-name");  
-        product_name.setAttribute('id',this.product.id+"_product_select");
+        product_name.setAttribute("id",this.product.id+"_product_select");
         product_name.innerHTML = this.product.name;
         product_div.appendChild(product_name);
         var color_div = document.createElement("div");                   //color div
         color_div.classList.add("color-box");
         container.appendChild(color_div);
-        var color_label = document.createElement('li');
+        var color_label = document.createElement("li");
         color_label.innerHTML = "Color";
         color_div.appendChild(color_label);
         color_label.classList.add("color");
@@ -47,7 +47,7 @@ var product_details = {
         var select_color = document.createElement("select");
         select_color.name = "color";
         select_color.classList.add(this.product.id);
-        select_color.setAttribute('id',this.product.id+"_color_select");
+        select_color.setAttribute("id",this.product.id+"_color_select");
         select_color.addEventListener("change", this.count_and_show_price);    
         select_color.addEventListener("change", order_summary.change_color);
         color_div.appendChild(select_color);
@@ -55,7 +55,7 @@ var product_details = {
         var capacity_div = document.createElement("div");               //capacity div
         capacity_div.classList.add("capacity-box");
         container.appendChild(capacity_div);
-        var capacity_label = document.createElement('li');
+        var capacity_label = document.createElement("li");
         capacity_label.innerHTML = "Capacity";
         capacity_div.appendChild(capacity_label);
         capacity_label.classList.add("capacity");
@@ -63,15 +63,15 @@ var product_details = {
         var select_capacity = document.createElement("select");
         select_capacity.name = "capacity";
         select_capacity.classList.add(this.product.id);
-        select_capacity.setAttribute('id', this.product.id+'_capacity_select')
+        select_capacity.setAttribute("id", this.product.id+"_capacity_select")
         select_capacity.addEventListener("change", this.count_and_show_price);
         select_capacity.addEventListener("change", order_summary.change_capacity);
         capacity_div.appendChild(select_capacity);
         this.create_selection_options(select_capacity, 1);
-        var quantity_div = document.createElement('div');               //quantity div
-        quantity_div.classList.add('quantity-box');
+        var quantity_div = document.createElement("div");               //quantity div
+        quantity_div.classList.add("quantity-box");
         container.appendChild(quantity_div);
-        var quantity_label = document.createElement('li');
+        var quantity_label = document.createElement("li");
         quantity_label.innerHTML = "Quantity";
         quantity_div.appendChild(quantity_label);
         quantity_label.classList.add("quantity");
@@ -79,13 +79,13 @@ var product_details = {
         var select_quantity = document.createElement("select");
         select_quantity.name = "quantity";
         select_quantity.classList.add(this.product.id);
-        select_quantity.setAttribute('id', this.product.id+'_quantity_select')
+        select_quantity.setAttribute("id", this.product.id+"_quantity_select")
         select_quantity.addEventListener("change", this.count_and_show_price);
         select_quantity.addEventListener("change", order_summary.change_quantity);
         quantity_div.appendChild(select_quantity);
         this.create_selection_options(select_quantity, 2);
-        var price_div = document.createElement('div');                  //price div
-        price_div.classList.add('price-box');
+        var price_div = document.createElement("div");                  //price div
+        price_div.classList.add("price-box");
         container.appendChild(price_div);
         var price_label = document.createElement("li");
         price_label.classList.add("small-screen-label");
@@ -103,17 +103,17 @@ var product_details = {
     },
     
     display_total_price: function () {
-        var total_price = document.createElement('p');
+        var total_price = document.createElement("p");
         total_price.innerHTML = "Total price: ";
-        total_price.setAttribute('id', 'total_price');
-        var parent = document.getElementById('order-cart');
+        total_price.setAttribute("id", "total_price");
+        var parent = document.getElementById("order-cart");
         parent.appendChild(total_price);
         product_details.count_and_show_total_price();
     },
     
     create_selection_options: function (select_parameter, i) { // i => parameter index from json
         for (let option of this.product.options[i].values){
-            var option_select = document.createElement('option');
+            var option_select = document.createElement("option");
             option_select.value = option.name;
             option_select.innerHTML = option.name;
             select_parameter.appendChild(option_select);
@@ -142,7 +142,7 @@ var product_details = {
     count_and_show_total_price: function () {
         var subtotal = 0;
         for (i=1; i<=order_data.length; i++) {
-            var part_price = document.getElementById(i+'_price').innerHTML;
+            var part_price = document.getElementById(i+"_price").innerHTML;
             part_price = parseFloat(part_price.slice(2,20));
             subtotal += part_price;
         }
@@ -188,11 +188,11 @@ var payment_form = {
         if (isValid) {
             order_summary.set_user_data(); // TO WRITE
             order_summary.dispatch_order();
-            document.getElementById('required_fields_alert').style.visibility="hidden";
+            document.getElementById("required_fields_alert").style.visibility="hidden";
             this.highlight_required_fields();
         }
         else {
-            document.getElementById('required_fields_alert').style.visibility="visible";
+            document.getElementById("required_fields_alert").style.visibility="visible";
             this.highlight_required_fields();
         }
     },
@@ -274,7 +274,7 @@ var order_summary = {
                 color = 1000; 
                 break;
         }
-        var product_index = this.getAttribute('class')-1; 
+        var product_index = this.getAttribute("class")-1; 
         order_summary.order[product_index].options[0].value = color;
     },
     
@@ -291,7 +291,7 @@ var order_summary = {
                 capacity = 1100;
                 break;
         }
-        var product_index = this.getAttribute('class')-1;
+        var product_index = this.getAttribute("class")-1;
         order_summary.order[product_index].options[1].value = capacity;
     },
     
@@ -318,21 +318,21 @@ var order_summary = {
                 quatity = 1200;
                 break;
         }
-        var product_index = this.getAttribute('class')-1;
+        var product_index = this.getAttribute("class")-1;
         order_summary.order[product_index].options[2].value = quantity;
    },
     
     
     set_user_data: function () {
         var user_data = {
-            name: '',
-            surname: '',
-            email: '',
+            name: "",
+            surname: "",
+            email: "",
             address: {
-                    street: '',
-                    houseNumber: '',
-                    city: '',
-                    postcode: ''
+                    street: "",
+                    houseNumber: "",
+                    city: "",
+                    postcode: ""
             }
         };
         var temp_value = "";
@@ -363,8 +363,8 @@ var order_summary = {
     },
         
     show_success: function () {
-        document.getElementById('sumbit-form-btn').disabled = true;
-        document.getElementById('success_alert').style.visibility="visible";
+        document.getElementById("sumbit-form-btn").disabled = true;
+        document.getElementById("success_alert").style.visibility="visible";
     }
 };
 
@@ -375,5 +375,4 @@ window.onload = function () {
     }
     init();
 });
-
 };
